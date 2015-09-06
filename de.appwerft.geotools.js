2eb358989fc85da078b4fe55f9b25dc6788aaaf5
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-09-06 19:14:55 +0200

          

--- de.appwerft.geotools.js
+++ de.appwerft.geotools.js
@@ -194,7 +194,8 @@ exports.loadKML = function() {
 		onload : function() {
 			var res = {
 				points : [],
-				linestrings : []
+				lines : [],
+				polygones : []
 			};
 			var placemarklist = this.responseXML.documentElement.getElementsByTagName("Placemark");
 			for (var placemarklistindex = 0; placemarklistindex < placemarklist.length; placemarklistindex++) {
@@ -204,10 +205,12 @@ exports.loadKML = function() {
 					var coords = Point.getElementsByTagName('coordinates').item(0).text;
 					res.points.push({
 						name : placemarknode.getElementsByTagName('name').item(0).text,
+						description : placemarknode.getElementsByTagName('description').item(0).text,
 						latitude : coords.split(',')[1],
 						longitude : coords.split(',')[0]
 					});
 				}
+				/* now we can collect linestring: */
 				if ( LineString = placemarknode.getElementsByTagName("LineString")) {
 					var points = [];
 					var coords = LineString.getElementsByTagName('coordinates').item(0).text.split(' ');
@@ -221,12 +224,32 @@ exports.loadKML = function() {
 							});
 						}
 					}
-					res.linestrings.push({
+					res.lines.push({
 						name : placemarknode.getElementsByTagName('name').item(0).text,
+						description : placemarknode.getElementsByTagName('description').item(0).text,
 						points : points
 					});
 				}
-				/* now we can collect linestring: */
+				/* now we can collect Ploygones: */
+				if ( LineString = placemarknode.getElementsByTagName("Polygone")) {
+					var points = [];
+					var coords = LineString.getElementsByTagName('coordinates').item(0).text.split(' ');
+					var coords_length = coords.length;
+					for (var cc = 0; cc < coords_length; cc++) {
+						loc = coords[cc].split(',');
+						if (loc[0] && loc[1]) {
+							points.push({
+								latitude : loc[1],
+								longitude : loc[0]
+							});
+						}
+					}
+					res.lines.push({
+						name : placemarknode.getElementsByTagName('name').item(0).text,
+						description : placemarknode.getElementsByTagName('description').item(0).text,
+						points : points
+					});
+				}
 
 			}
 		},
