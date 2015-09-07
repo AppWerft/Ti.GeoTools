222062440b42554ede23eaaecfeb47cde85160a9
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-09-07 13:35:28 +0200

    bugfixing

--- de.appwerft.geotools.js
+++ de.appwerft.geotools.js
@@ -17,7 +17,7 @@ var Promise = require('org.favo.promise');
 exports.getPositionByIP = function(_ip) {
 	var ip = _ip ? _ip : Ti.Platform.getAddress();
 	var promise = Promise.defer();
-	var xhr = Ti.UI.createHTTPClient({
+	var xhr = Ti.Network.createHTTPClient({
 		onload : function() {
 			promise.resolve(JSON.parse(this.responseText));
 		},
@@ -35,7 +35,7 @@ exports.getElevation = function() {
 	var φ = Array.isArray(position) ? position[0] : position.lat || position.latitude;
 	var λ = Array.isArray(position) ? position[1] : position.lng || position.lon || position.longitude;
 	var promise = Promise.defer();
-	var xhr = Ti.UI.createHTTPClient({
+	var xhr = Ti.Network.createHTTPClient({
 		onload : function() {
 			promise.resolve(JSON.parse(this.responseText));
 		},
@@ -51,7 +51,7 @@ exports.getElevation = function() {
 exports.getRegionOfCountry = function(_country) {
 	var promise = Promise.defer();
 	var country = _country || 'Deutschland';
-	var xhr = Ti.UI.createHTTPClient({
+	var xhr = Ti.Network.createHTTPClient({
 		onload : function() {
 			try {
 				var json = JSON.parse(this.responseText);
@@ -156,13 +156,12 @@ exports.getRoute = function() {
 	return promise;
 };
 
-
-
 exports.loadKML = function() {
 	var url = arguments[0];
 	var promise = Promise.defer();
 	var xhr = Ti.Network.createHTTPClient({
 		onload : function() {
+			var start = new Date().getTime();
 			var res = {
 				points : [],
 				lines : [],
@@ -172,19 +171,22 @@ exports.loadKML = function() {
 			for (var placemarklistindex = 0; placemarklistindex < placemarklist.length; placemarklistindex++) {
 				var placemarknode = placemarklist.item(placemarklistindex);
 				/* now we can collect points: */
-				if ( Point = placemarknode.getElementsByTagName("Point")) {
-					var coords = Point.getElementsByTagName('coordinates').item(0).text;
+				var Points = placemarknode.getElementsByTagName("Point");
+				if (Points) {
+					var coords = Points.item(0).getElementsByTagName('coordinates').item(0).textContent;
+					var description = placemarknode.getElementsByTagName('description').item(0);
 					res.points.push({
-						name : placemarknode.getElementsByTagName('name').item(0).text,
-						description : placemarknode.getElementsByTagName('description').item(0).text,
+						name : placemarknode.getElementsByTagName('name').item(0).textContent,
+						description : description ? description.textContent : undefined,
 						latitude : coords.split(',')[1],
 						longitude : coords.split(',')[0]
 					});
 				}
-				/* now we can collect linestring: */
-				if ( LineString = placemarknode.getElementsByTagName("LineString")) {
+				/* now we can collect linestrings: */
+				var LineStrings = placemarknode.getElementsByTagName("LineString");
+				if (LineStrings && LineStrings.item(0)) {
 					var points = [];
-					var coords = LineString.getElementsByTagName('coordinates').item(0).text.split(' ');
+					var coords = LineStrings.item(0).getElementsByTagName('coordinates').item(0).textContent.split(' ');
 					var coords_length = coords.length;
 					for (var cc = 0; cc < coords_length; cc++) {
 						loc = coords[cc].split(',');
@@ -195,16 +197,18 @@ exports.loadKML = function() {
 							});
 						}
 					}
+					var description = placemarknode.getElementsByTagName('description').item(0);
 					res.lines.push({
-						name : placemarknode.getElementsByTagName('name').item(0).text,
-						description : placemarknode.getElementsByTagName('description').item(0).text,
+						name : placemarknode.getElementsByTagName('name').item(0).textContent,
+						description : description ? description.textContent : undefined,
 						points : points
 					});
 				}
-				/* now we can collect Ploygones: */
-				if ( LineString = placemarknode.getElementsByTagName("Polygone")) {
+				/* now we can collect Polygones: */
+				var Polygons = placemarknode.getElementsByTagName("Polygon");
+				if (Polygons && Polygons.item(0)) {
 					var points = [];
-					var coords = LineString.getElementsByTagName('coordinates').item(0).text.split(' ');
+					var coords = Polygons.item(0).getElementsByTagName('coordinates').item(0).textContent.split(' ');
 					var coords_length = coords.length;
 					for (var cc = 0; cc < coords_length; cc++) {
 						loc = coords[cc].split(',');
@@ -215,14 +219,16 @@ exports.loadKML = function() {
 							});
 						}
 					}
+					var description = placemarknode.getElementsByTagName('description').item(0);
 					res.lines.push({
-						name : placemarknode.getElementsByTagName('name').item(0).text,
-						description : placemarknode.getElementsByTagName('description').item(0).text,
+						name : placemarknode.getElementsByTagName('name').item(0).textContent,
+						description : description ? description.textContent : undefined,
 						points : points
 					});
 				}
-
 			}
+			console.log('Info: KML parsingtime: ' + (new Date().getTime() - start));
+			promise.resolve(res);
 		},
 		onerror : function(_e) {
 			promise.reject(_e);
@@ -341,115 +347,116 @@ exports.GaussKrueger2Geo = function() {
 };
 
 exports.UTM2Geo = function(zone, ew, nw) {
-    /* Copyright (c) 2006, HELMUT H. HEIMEIER
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included
-    in all copies or substantial portions of the Software.*/
+	/* Copyright (c) 2006, HELMUT H. HEIMEIER
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+	The above copyright notice and this permission notice shall be included
+	in all copies or substantial portions of the Software.*/
 
-    /* Die Funktion wandelt UTM Koordinaten in geographische Koordinaten
-    um. UTM Zone, Ostwert ew und Nordwert nw müssen gegeben sein.
-    Berechnet werden geographische Länge lw und Breite bw im WGS84 Datum.*/
+	/* Die Funktion wandelt UTM Koordinaten in geographische Koordinaten
+	um. UTM Zone, Ostwert ew und Nordwert nw müssen gegeben sein.
+	Berechnet werden geographische Länge lw und Breite bw im WGS84 Datum.*/
 
-    // Längenzone zone, Ostwert ew und Nordwert nw im WGS84 Datum
-    if (zone == "" || ew == "" || nw == "") {
-        zone = "";
-        ew = "";
-        nw = "";
-        return;
-    }
-    band = zone.substr(2, 1);
-    zone = parseFloat(zone);
-    ew = parseFloat(ew);
-    nw = parseFloat(nw);
+	// Längenzone zone, Ostwert ew und Nordwert nw im WGS84 Datum
+	if (zone == "" || ew == "" || nw == "") {
+		zone = "";
+		ew = "";
+		nw = "";
+		return;
+	}
+	band = zone.substr(2, 1);
+	zone = parseFloat(zone);
+	ew = parseFloat(ew);
+	nw = parseFloat(nw);
 
-    // WGS84 Datum
-    // Große Halbachse a und Abplattung f
-    a = 6378137.000;
-    f = 3.35281068e-3;
-    pi = Math.PI;
+	// WGS84 Datum
+	// Große Halbachse a und Abplattung f
+	a = 6378137.000;
+	f = 3.35281068e-3;
+	pi = Math.PI;
 
-    // Polkrümmungshalbmesser c
-    c = a / (1 - f);
+	// Polkrümmungshalbmesser c
+	c = a / (1 - f);
 
-    // Quadrat der zweiten numerischen Exzentrizität
-    ex2 = (2 * f - f * f) / ((1 - f) * (1 - f));
-    ex4 = ex2 * ex2;
-    ex6 = ex4 * ex2;
-    ex8 = ex4 * ex4;
+	// Quadrat der zweiten numerischen Exzentrizität
+	ex2 = (2 * f - f * f) / ((1 - f) * (1 - f));
+	ex4 = ex2 * ex2;
+	ex6 = ex4 * ex2;
+	ex8 = ex4 * ex4;
 
-    // Koeffizienten zur Berechnung der geographischen Breite aus gegebener
-    // Meridianbogenlänge
-    e0 = c * (pi / 180) * (1 - 3 * ex2 / 4 + 45 * ex4 / 64 - 175 * ex6 / 256 + 11025 * ex8 / 16384);
-    f2 = (180 / pi) * (3 * ex2 / 8 - 3 * ex4 / 16 + 213 * ex6 / 2048 - 255 * ex8 / 4096);
-    f4 = (180 / pi) * (21 * ex4 / 256 - 21 * ex6 / 256 + 533 * ex8 / 8192);
-    f6 = (180 / pi) * (151 * ex6 / 6144 - 453 * ex8 / 12288);
+	// Koeffizienten zur Berechnung der geographischen Breite aus gegebener
+	// Meridianbogenlänge
+	e0 = c * (pi / 180) * (1 - 3 * ex2 / 4 + 45 * ex4 / 64 - 175 * ex6 / 256 + 11025 * ex8 / 16384);
+	f2 = (180 / pi) * (3 * ex2 / 8 - 3 * ex4 / 16 + 213 * ex6 / 2048 - 255 * ex8 / 4096);
+	f4 = (180 / pi) * (21 * ex4 / 256 - 21 * ex6 / 256 + 533 * ex8 / 8192);
+	f6 = (180 / pi) * (151 * ex6 / 6144 - 453 * ex8 / 12288);
 
-    // Entscheidung Nord-/Süd Halbkugel
-    if (band >= "N" || band == "")
-        m_nw = nw
-    else
-        m_nw = nw - 10e6;
+	// Entscheidung Nord-/Süd Halbkugel
+	if (band >= "N" || band == "")
+		m_nw = nw
+	else
+		m_nw = nw - 10e6;
 
-    // Geographische Breite bf zur Meridianbogenlänge gf = m_nw
-    sigma = (m_nw / 0.9996) / e0;
-    sigmr = sigma * pi / 180;
-    bf = sigma + f2 * Math.sin(2 * sigmr) + f4 * Math.sin(4 * sigmr) + f6 * Math.sin(6 * sigmr);
+	// Geographische Breite bf zur Meridianbogenlänge gf = m_nw
+	sigma = (m_nw / 0.9996) / e0;
+	sigmr = sigma * pi / 180;
+	bf = sigma + f2 * Math.sin(2 * sigmr) + f4 * Math.sin(4 * sigmr) + f6 * Math.sin(6 * sigmr);
 
-    // Breite bf in Radianten
-    br = bf * pi / 180;
-    tan1 = Math.tan(br);
-    tan2 = tan1 * tan1;
-    tan4 = tan2 * tan2;
+	// Breite bf in Radianten
+	br = bf * pi / 180;
+	tan1 = Math.tan(br);
+	tan2 = tan1 * tan1;
+	tan4 = tan2 * tan2;
 
-    cos1 = Math.cos(br);
-    cos2 = cos1 * cos1;
+	cos1 = Math.cos(br);
+	cos2 = cos1 * cos1;
 
-    etasq = ex2 * cos2;
+	etasq = ex2 * cos2;
 
-    // Querkrümmungshalbmesser nd
-    nd = c / Math.sqrt(1 + etasq);
-    nd2 = nd * nd;
-    nd4 = nd2 * nd2;
-    nd6 = nd4 * nd2;
-    nd3 = nd2 * nd;
-    nd5 = nd4 * nd;
+	// Querkrümmungshalbmesser nd
+	nd = c / Math.sqrt(1 + etasq);
+	nd2 = nd * nd;
+	nd4 = nd2 * nd2;
+	nd6 = nd4 * nd2;
+	nd3 = nd2 * nd;
+	nd5 = nd4 * nd;
 
-    // Längendifferenz dl zum Bezugsmeridian lh
-    lh = (zone - 30) * 6 - 3;
-    dy = (ew - 500000) / 0.9996;
-    dy2 = dy * dy;
-    dy4 = dy2 * dy2;
-    dy3 = dy2 * dy;
-    dy5 = dy3 * dy2;
-    dy6 = dy3 * dy3;
+	// Längendifferenz dl zum Bezugsmeridian lh
+	lh = (zone - 30) * 6 - 3;
+	dy = (ew - 500000) / 0.9996;
+	dy2 = dy * dy;
+	dy4 = dy2 * dy2;
+	dy3 = dy2 * dy;
+	dy5 = dy3 * dy2;
+	dy6 = dy3 * dy3;
 
-    b2 = -tan1 * (1 + etasq) / (2 * nd2);
-    b4 = tan1 * (5 + 3 * tan2 + 6 * etasq * (1 - tan2)) / (24 * nd4);
-    b6 = -tan1 * (61 + 90 * tan2 + 45 * tan4) / (720 * nd6);
+	b2 = -tan1 * (1 + etasq) / (2 * nd2);
+	b4 = tan1 * (5 + 3 * tan2 + 6 * etasq * (1 - tan2)) / (24 * nd4);
+	b6 = -tan1 * (61 + 90 * tan2 + 45 * tan4) / (720 * nd6);
 
-    l1 = 1 / (nd * cos1);
-    l3 = -(1 + 2 * tan2 + etasq) / (6 * nd3 * cos1);
-    l5 = (5 + 28 * tan2 + 24 * tan4) / (120 * nd5 * cos1);
+	l1 = 1 / (nd * cos1);
+	l3 = -(1 + 2 * tan2 + etasq) / (6 * nd3 * cos1);
+	l5 = (5 + 28 * tan2 + 24 * tan4) / (120 * nd5 * cos1);
 
-    // Geographische Breite bw und Länge lw als Funktion von Ostwert ew
-    // und Nordwert nw
-    bw = bf + (180 / pi) * (b2 * dy2 + b4 * dy4 + b6 * dy6);
-    lw = lh + (180 / pi) * (l1 * dy + l3 * dy3 + l5 * dy5);
-    return {
-        latitude : bw,
-        longitude : lw
-    };
-}; 
+	// Geographische Breite bw und Länge lw als Funktion von Ostwert ew
+	// und Nordwert nw
+	bw = bf + (180 / pi) * (b2 * dy2 + b4 * dy4 + b6 * dy6);
+	lw = lh + (180 / pi) * (l1 * dy + l3 * dy3 + l5 * dy5);
+	return {
+		latitude : bw,
+		longitude : lw
+	};
+};
 
 exports.LatLon = function(lat, lon) {
-    // allow instantiation without 'new'
-    if (!(this instanceof LatLon)) return new LatLon(lat, lon);
-    this.lat = Number(lat);
-    this.lon = Number(lon);
-    return this;
+	// allow instantiation without 'new'
+	if (!(this instanceof LatLon))
+		return new LatLon(lat, lon);
+	this.lat = Number(lat);
+	this.lon = Number(lon);
+	return this;
 }
